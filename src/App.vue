7d7da0efeea6bc961833f6251ed781f3e197ca4e
gabrielimarta <gabrielirocha2730@gmail.com>
2024-07-15 13:49:58 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import ExpandBox from './components/ExpandBox.vue'
+</script>
+
+<template>
+<h1>Componentes</h1>
+<ExpandBox />
+<br>
+<expand-box />
+<br>
+<ExpandBox />
+
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/ExpandBox.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue';
+const showContent = ref(false)
+</script>
+
+<template>
+    <div><button @click="showContent = !showContent">mostrar</button></div>
+    <div v-if="showContent" class="expand-box">beijinho no ombro</div>
+</template>
+
+<style scoped>
+.expand-box {
+    height: 80%;
+    padding: 20px;
+    border: 1 px solid black;
+    box-shadow: 2px 2px 3px 2px gray;
+    border-radius: 10px;
+}
+</style>
